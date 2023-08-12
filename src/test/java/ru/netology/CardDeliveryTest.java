d58381485671e0ac2a4b17343b933825dd425e74
unknown <ketvolk@mail.ru>
2023-08-12 14:12:44 +0300

    Test changed

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -6,44 +6,37 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-
     @Test
     void shouldDeliverCardAgain() {
 
         open("http://localhost:9999/");
-        var city = DataClass.generateCity();
+
         var planningDayFirst = DataClass.generateDate();
         var planningDaySecond = DataClass.generateDate();
-        var name = DataClass.generateName();
-        var phone = DataClass.generatePhone();
-        DataClass data1 = new DataClass(city, planningDayFirst, name, phone);
 
-        $("[data-test-id='city'] input").setValue(data1.getCity());
+        $("[data-test-id='city'] input").setValue(DataClass.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(data1.getDate());
-        $("[data-test-id='name'] input").setValue(data1.getName());
-        $("[data-test-id='phone'] input").setValue(data1.getPhone());
+        $("[data-test-id='date'] input").setValue(planningDayFirst);
+        $("[data-test-id='name'] input").setValue(DataClass.generateName());
+        $("[data-test-id='phone'] input").setValue(DataClass.generatePhone());
         $("[data-test-id='agreement']").click();
         $("[class='button__content']").click();
         $(".notification__title").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15)).shouldBe(visible);
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDayFirst), Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно запланирована на " + planningDayFirst));
+        $(".icon-button.notification__closer").click();
 
-        open("http://localhost:9999/");
-        DataClass data2 = new DataClass(city, planningDaySecond, name, phone);
-        $("[data-test-id='city'] input").setValue(data2.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(data2.getDate());
-        $("[data-test-id='name'] input").setValue(data2.getName());
-        $("[data-test-id='phone'] input").setValue(data2.getPhone());
-        $("[data-test-id='agreement']").click();
+        $("[data-test-id='date'] input").setValue(planningDaySecond);
         $("[class='button__content']").click();
-        $(".notification__title").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15)).shouldBe(visible);
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDaySecond), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id=replan-notification] .notification__title").shouldBe(visible).shouldHave(exactText("Необходимо подтверждение"));
+        $("[data-test-id=replan-notification] .button").click();
+        $(".notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно запланирована на " + planningDaySecond));
     }
 }
